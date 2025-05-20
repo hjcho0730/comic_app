06536832e3c918cfddba1f4983e840929e601bba
hjcho <hjcho9730@gmail.com>
2025-05-21 01:37:28 +0900

    시작

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import { FaArrowAltCircleDown, FaArrowAltCircleUp } from "react-icons/fa";
+import { useRef } from "react";
 import './App.css';
 
 function App() {
+  const pages = 4;
+
+  const arrLoop = () => {
+    const newArr = [];
+    for (let i = 1; i <= pages; i++) {
+      newArr.push(
+        <center>
+          <img
+            key={i}
+            className='picture'
+            src={process.env.PUBLIC_URL + '/images/picture' + i + '.png'}
+            alt=""
+            />
+          </center>
+          );
+    }
+    return newArr;
+  };
+
+  const scrollRef = useRef();
+
+  function down() {
+    scrollRef.current.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+  }
+
+  function top() {
+    scrollRef.current.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" ref={scrollRef}>
+      <h1>문버드 만화</h1>
+      <h4>작가: 작가</h4>
+      {arrLoop()}
+      <div className='abs'>
+        <button className='btn' onClick={top}><FaArrowAltCircleUp className="icon"/></button>
+        <button className='btn' onClick={down}><FaArrowAltCircleDown className="icon"/></button>
+      </div>
     </div>
   );
 }
-
 export default App;
